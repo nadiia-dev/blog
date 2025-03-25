29a301293d4bd0b6a95bc56b502b962cba67d56b
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-25 17:24:07 +0200

    make markdown posts dispaly images

--- components/posts/post-detail/PostDetail.tsx
+++ components/posts/post-detail/PostDetail.tsx
@@ -1,13 +1,32 @@
 import { Post } from "@/types/Post";
 import PostHeader from "./PostHeader";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 const PostDetail = ({ post }: { post: Post }) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customRenderers = {
+    p({ node, children }) {
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className="my-4 mx-auto w-full max-w-[600px]">
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{children}</p>;
+    },
+  };
   return (
     <article className="w-[95%] max-w-[60rem] my-8 mx-auto text-base leading-8 bg-gray-100 rounded-lg p-4 sm:p-8">
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
