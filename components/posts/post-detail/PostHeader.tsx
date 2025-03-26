9e23d3da0e6e8f995455f8b929790d6d44710f48
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-26 18:35:06 +0200

    handle toggle like posts

--- components/posts/post-detail/PostHeader.tsx
+++ components/posts/post-detail/PostHeader.tsx
@@ -1,18 +1,69 @@
+import { idenifyUser } from "@/util/identify";
+import clsx from "clsx";
 import Image from "next/image";
+import { useState } from "react";
+import { BiSolidLike } from "react-icons/bi";
 
-const PostHeader = ({ title, image }: { title: string; image: string }) => {
+const PostHeader = ({
+  postSlug,
+  title,
+  image,
+}: {
+  postSlug: string;
+  title: string;
+  image: string;
+}) => {
+  const [liked, setLiked] = useState(false);
+  const toggleLike = async () => {
+    const userFingerprint = await idenifyUser();
+    try {
+      const res = await fetch("/api/like", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          post_slug: postSlug,
+          comment_id: null,
+          user_fingerprint: userFingerprint,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (data.message === "Like added") {
+        setLiked(true);
+      } else if (data.message === "Like removed") {
+        setLiked(false);
+      }
+    } catch (error) {
+      console.error("Error toggling like:", error);
+    }
+  };
+
   return (
-    <header className="pb-8 border-b-8 border-primary-100 my-4 flex flex-col-reverse justify-between items-center gap-4 md:my-8 md:flex-row md:items-end">
-      <h1 className="text-2xl m-0 leading-none text-center md:text-6xl md:text-left">
-        {title}
-      </h1>
-      <Image
-        className="object-cover w-50 h-30"
-        src={image}
-        alt={title}
-        width={200}
-        height={150}
-      />
+    <header className="pb-8 border-b-8 border-primary-100 my-4">
+      <div className="flex flex-col-reverse justify-between items-center gap-4 md:my-8 md:flex-row md:items-end">
+        <h1 className="text-2xl m-0 leading-none text-center md:text-6xl md:text-left">
+          {title}
+        </h1>
+        <Image
+          className="object-cover w-50 h-30"
+          src={image}
+          alt={title}
+          width={200}
+          height={150}
+        />
+      </div>
+      <div className="flex gap-2.5 items-center">
+        <button onClick={toggleLike}>
+          <BiSolidLike
+            size={30}
+            className={clsx(liked && "text-red-600", !liked && "text-gray-950")}
+          />
+        </button>
+        <p>Liked</p>
+      </div>
     </header>
   );
 };
---
+++ pages/api/like.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { post_slug, comment_id, user_fingerprint } = req.body;
+
+    try {
+      const likes = await sql`
+    SELECT * FROM likes WHERE post_slug = ${post_slug} AND user_fingerprint = ${user_fingerprint} AND (comment_id = ${comment_id} OR comment_id IS NULL);
+  `;
+      if (likes.length > 0) {
+        await sql`
+        DELETE FROM likes WHERE post_slug = ${post_slug} AND user_fingerprint = ${user_fingerprint} AND (comment_id = ${comment_id} OR comment_id IS NULL);;
+      `;
+        return res.json({ message: "Like removed" });
+      } else {
+        await sql`
+        INSERT INTO likes (post_slug, comment_id, user_fingerprint) VALUES (${post_slug}, ${comment_id}, ${user_fingerprint});
+      `;
+        return res.json({ message: "Like added" });
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        console.error(e);
+        res.status(500).json({ message: "Error processing like" });
+      }
+    }
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
